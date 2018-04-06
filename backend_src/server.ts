66e8b667d6d5d5edef6f1d723d24eb844cb607bb
Robert Herley <rherley@stevens.edu>
2018-04-06 15:24:25 -0400

    i did things... unspeakable things

--- backend_src/server.ts
+++ backend_src/server.ts
@@ -1,48 +1,60 @@
-import * as bodyParser from 'body-parser';
-import * as cors from 'cors';
-import * as express from 'express';
-import * as mongoose from 'mongoose';
-import * as logger from 'morgan';
+import cors from 'cors';
+import express from 'express';
+import morgan from 'morgan';
 
 class Server {
-  public app: express.Application;
+   public app: express.Application;
 
-  constructor() {
-    this.app = express();
-    this.config();
-    this.routes();
-  }
+   constructor() {
+      this.app = express();
+      this.config();
+      this.routes();
+   }
 
-  private config(): void {
-    // connect mongoose
-    const MONGO_URI = '';
+   private config(): void {
+      // connect mongoose
+      const MONGO_URI = '';
 
-    // configuration
-    this.app.use(bodyParser.json());
-    this.app.use(bodyParser.urlencoded({ extended: true }));
-    this.app.use(logger('dev'));
+      // configuration
+      this.app.use(express.json());
+      this.app.use(express.urlencoded({ extended: true }));
+      this.app.use(morgan('dev'));
 
-    // cors
-    this.app.use(cors());
-    this.app.use((req, res, next) => {
-      res.header('Access-Control-Allow-Origin', 'http://localhost:8080');
-      res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-      res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Credentials');
-      res.header('Access-Control-Allow-Credentials', 'true');
-      next();
-    });
-  }
+      // cors
+      this.app.use(cors());
+      this.app.use(
+         (
+            req: express.Request,
+            res: express.Response,
+            next: express.NextFunction
+         ) => {
+            res.header('Access-Control-Allow-Origin', 'http://localhost:8080');
+            res.header(
+               'Access-Control-Allow-Methods',
+               'GET, POST, PUT, DELETE, OPTIONS'
+            );
+            res.header(
+               'Access-Control-Allow-Headers',
+               'Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Credentials'
+            );
+            res.header('Access-Control-Allow-Credentials', 'true');
+            next();
+         }
+      );
+   }
 
-  private routes(): void {
-    let router: express.Router;
-    router = express.Router();
+   private routes(): void {
+      let router: express.Router;
+      router = express.Router();
 
-    this.app.use('/', router);
+      // this.app.use('/', router);
 
-    this.app.use('*', (req, res) => {
-      res.status(404).json({ error: 'non existant' });
-    });
-  }
+      this.app.use('*', (req: express.Request, res: express.Response) => {
+         res
+            .status(418)
+            .json({ error: `i'm a teapot`, meme: 'https://http.cat/420' });
+      });
+   }
 }
 
 export default new Server().app;
