2d992050ad8143841f07b7e3fa6a86bce247edc4
Jonathan Pavlik <jonathanpavlik@Jonathan-Pavlik-MBP.home>
2018-04-07 16:17:08 -0400

    only the one ts error left

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -10,7 +10,7 @@ export interface IUser extends Document {
   username: string,
   email: string,
   password: string,
-  newUser(newUser: any, callback: any): any
+  newUser(newUser: IUser, callback: Function): Promise<any>
 }
 
 const UserSchema: Schema = new Schema({
@@ -52,11 +52,7 @@ const UserSchema: Schema = new Schema({
 
 UserSchema.plugin(passportLocalMongoose);
 
-const User: Model<IUser> = model<IUser>('User', UserSchema);
-
-export default User;
-
-UserSchema.method('newUser', async (newUser: any, callback: any) => {
+UserSchema.method('newUser', async (newUser: any, callback: Function): Promise<any> => {
   bcrypt.genSalt(10, async (err: Error | null, salt: string) => {
     bcrypt.hash(newUser.password, salt, async (err: Error | null, hash: string) => {
       newUser.password = hash;
@@ -65,22 +61,22 @@ UserSchema.method('newUser', async (newUser: any, callback: any) => {
   });
 });
 
-module.exports.getUserByUsername = (username: string, callback) => {
+module.exports.getUserByUsername = (username: string, callback: Function) => {
   User.findOne({username: username}, callback);
 }
 
-module.exports.getUserById =  (id: string, callback) => {
+module.exports.getUserById =  (id: string, callback: Function) => {
   User.findById(id, callback);
 }
 
-module.exports.comparePassword =  (possiblePass: string, hash: string, callback) => {
+module.exports.comparePassword =  (possiblePass: string, hash: string, callback: Function) => {
   bcrypt.compare(possiblePass, hash,  (err: Error | null, match: boolean) => {
     if(err) throw err;
     callback(null, match);
   });
 }
 
-module.exports.updateUser = async (id: string, updates, callback) => {
+module.exports.updateUser = async (id: string, updates, callback: any) => {
   if(updates.password) {
     await bcrypt.genSalt(10, async (err: Error | null, salt: string) => {
       await bcrypt.hash(updates.password, salt, async (err: Error | null, hash: string | number) => {
@@ -107,3 +103,7 @@ module.exports.updateUser = async (id: string, updates, callback) => {
   }
 
 };
+
+const User: Model<IUser> = model<IUser>('User', UserSchema);
+
+export default User;
